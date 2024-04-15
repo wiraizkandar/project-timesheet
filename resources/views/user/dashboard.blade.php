52d02b76b2ceee4bf42acefae331d2bdccdfba1d
wiraizkandar <wiraizkandar1@gmail.com>
2024-04-15 10:03:57 +0800

    change user dashboard intro test

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -4,7 +4,7 @@
 @endsection
 @section('content')
     <div class="flex flex-col justify-center items-center space-x-4 space-y-4  w-1/4 mx-auto">
-        <h1 class="text-2xl font-semibold text-gray-900">Admin Dashboard</h1>
+        <h1 class="text-2xl font-semibold text-gray-900">User Dashboard</h1>
 
     </div>
 @stop
